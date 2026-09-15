46a75b57d3bf35b1fa32799e383bad3939dbac7f
Johan Lundberg <lundberg@sunet.se>
2026-09-15 11:46:56 +0200

    chore: update ghdiff version

--- Formula/ghdiff.rb
+++ Formula/ghdiff.rb
@@ -1,8 +1,8 @@
 class Ghdiff < Formula
   desc "Git diff viewer in GitHub-style web UI"
   homepage "https://github.com/johanlundberg/ghdiff"
-  url "https://github.com/johanlundberg/ghdiff/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "bef140a1a96994029153dca8c00b1750b9a5a764fb9db2dc68d7bb40e8a29e8a"
+  url "https://github.com/johanlundberg/ghdiff/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "9ff2cc0fa5e046babfd0067e33f813e46d22af2af91eb731df51b3d65d7940e3"
   license "BSD-2-Clause"
 
   depends_on "go" => :build
